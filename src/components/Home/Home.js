14768b68935841b1be303a9e367e2df2135fa352
Epinder Singh Mann <epinder.singh@gmail.com>
2021-12-25 17:05:14 +0800

    Error in Movie details

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,18 @@
 import React, { useEffect } from 'react';
 import MovieListing from '../MovieListing/MovieListing';
-import movieApi from '../../common/apis/MovieApi';
-import { APIKey } from '../../common/apis/MovieApiKey';
+
+import { useDispatch } from 'react-redux';
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from '../../features/movies/movieSlice';
 
 const Home = () => {
+  const dispatch = useDispatch();
   useEffect(() => {
-    const movieText = 'Harry';
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log('Error', err);
-        });
-      console.log('The Response from API: ', response);
-    };
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
+  }, [dispatch]);
   return (
     <div>
       <div className='banner-img'></div>
